e49b2aab6eab89a53371f756b3b5e26cefa2c6df
Valentin <samuelvalentin5013@gmail.com>
2024-10-19 08:40:27 -0500

    first commit

---
+++ src/test/java/com/hospitaldb/hospital/HospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitaldb.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
